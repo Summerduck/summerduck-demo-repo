af11aeb71fac2d0bb4cd5fd26016a971700dbabb
Summerduck <daria.samardak@gmail.com>
2024-03-25 10:10:26 +0200

    Create new files

---
+++ test_sample copy 4.py
@@ -0,0 +1,12 @@
+# content of test_sample.py
+def increment(x):
+    return x + 1
+
+def test_failed():
+    assert increment(3) != 5
+
+def test_passed():
+    assert increment(4) == 5
+
+def test_failed_2():
+    assert increment(7) == 5
